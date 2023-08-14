be3dcbe6198884b4c6e30ff7258e5905c97e935d
Luis Ley <proyectos@dley.com.ec>
2023-08-14 12:54:59 -0500

    Configuraciones Wifi

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,58 @@
 #include <Arduino.h>
+#include <ArduinoJson.h>
+#include <SPIFFS.h>
+
+#include "settings.hpp"
+#include "functions.hpp"
+#include "settingsReset.hpp"
+#include "settingsRead.hpp"
+#include "settingsSave.hpp"
+#include "esp32_wifi.hpp"
 
 
 void setup() {
+    // Baudrate
+    Serial.begin(115200);
+    // CPU Freq
+    setCpuFrequencyMhz(240);
+    // Inicio del Log por serial
+    log("\nInfo: Iniciando Setup");
+
+    // Configurar los Pines
+    settingsPines();
+    
+
+    // Inicio del SPIFFS                 
+    if (!SPIFFS.begin()){
+        log(F("Error: Falló la inicialización del SPIFFS"));
+        while (true);
+    }
+    // Lee la Configuración WiFi
+    settingsReadWiFi();
+
+    // Configuracion WIFI
+    WiFi.disconnect(true);
+    delay(1000);
+
+    // Setup del WiFI
+    wifi_setup(); 
+
 
 }
 
 void loop() {
+   yield();
+    // -------------------------------------------------------------------
+    // WIFI
+    // -------------------------------------------------------------------
+    if (wifi_mode == WIFI_STA){
+        wifiLoop();
+    }else if (wifi_mode == WIFI_AP){
+        wifiAPLoop();
+    } 
 
 }
+
+
+
 
